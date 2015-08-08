633fa0cb15f7b1def987667361e8ff58ed43e892
Kseniya <kalyuk.msn@gmail.com>
2015-08-07 20:57:15 -0400

    Added slides and finished project from Unity UI class

---
+++ Classes_and_Workshops/Unity_UI/HappyKitty finished/Assets/Scripts/TopBar.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TopBar : MonoBehaviour {
+
+	public Text pointsText;
+	public Image progressBar;
+
+	Kitty kitty;
+
+	// Use this for initialization
+	void Start () {
+		kitty = FindObjectOfType<Kitty> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		pointsText.text = kitty.points.ToString();
+		progressBar.fillAmount = kitty.timeLeft / kitty.maxTime;
+	}
+}
