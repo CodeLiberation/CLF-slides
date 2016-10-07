d939d1fe40365e5149cf94158fb474c0215c99d8
Phoenix Perry <contact@phoenixperry.com>
2016-10-07 18:25:27 +0100

    adding the first week slides

---
+++ Classes_and_Workshops/Cat_Sample/Assets/move_script.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class move_script : MonoBehaviour {
+
+public Vector3 myVect; 
+
+	// Use this for initialization
+	void Start () {
+		myVect = new Vector3(0.0f,0.0f,0.0f);
+	}	
+	
+	// Update is called once per frame
+	void Update () {
+ 
+		transform.Translate(myVect, Space.Self); 
+		myVect.x += .10f; 
+		transform.Rotate(myVect*360); 
+	}
+}
