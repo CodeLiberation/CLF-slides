4550d7c3154fe4ef82ea2fa1717d1d337b4367e8
Phoenix Perry <pperr010@gold.ac.uk>
2016-11-11 17:59:34 +0000

    adding week 04

---
+++ Classes_and_Workshops/V&A_Workshops/Unity_Arduino/Arduino_code/key_presses/key_presses.ino
@@ -0,0 +1,18 @@
+#include<Keyboard.h>
+int switchState = 0; 
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(2, INPUT); 
+  Serial.begin(9600); 
+  
+}
+void loop() {
+  // put your main code here, to run repeatedly:
+  int switchState = digitalRead(2); 
+  if(switchState == 1)
+  {
+    Keyboard.write('w');
+    delay(500);   
+  }
+Serial.println(switchState); 
+}
---
+++ Classes_and_Workshops/V&A_Workshops/Unity_Arduino/Arduino_code/readingSensors/readingSensors.ino
@@ -0,0 +1,18 @@
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600); 
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int values[] = {analogRead(0), analogRead(1)};
+  //Serial.flush; 
+  for(int i=0; i < 2; i++) 
+  {
+    Serial.print(values[i]); 
+    Serial.print(","); 
+    Serial.println(); 
+  //  delay(30); //I saw people say you needed this online but it seems to work w/out it just fine. 
+    Serial.flush();
+  } 
+}
