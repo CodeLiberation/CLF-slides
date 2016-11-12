e1022154720cc7c8930816a172203a70b00032f3
Phoenix Perry <pperr010@gold.ac.uk>
2016-11-12 15:17:14 +0000

    fixing files for week 4

---
+++ Classes_and_Workshops/V&A_Workshops/Unity_Arduino_4/Pro_micro_Keypress_Caat_Game/Assets/Mover.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mover : MonoBehaviour {
+    
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(Input.GetAxis("Horizontal"), 0.0f, 0.0f); 
+	}
+}
--- Classes_and_Workshops/V&A_Workshops/Unity_Arduino_4/Unity_arduino_4/README.md
+++
@@ -1,7 +0,0 @@
-#Week 04 
-Button link: 
-<https://www.arduino.cc/en/Tutorial/Button> 
-
-Keypress code: 
-<https://www.sparkfun.com/tutorials/337>
-
